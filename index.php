61c3c4451b2edcd31cbf39abdf8e259cf487d0cb
Paulo <Paulo@DESKTOP-7PV6HCU>
2018-04-17 18:46:45 -0300

    Adicionando o form de Login

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ and open the template in the editor.
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>Home</title>
         <style>
         body,h1,h5 {font-family: "Raleway", sans-serif}
@@ -24,7 +25,7 @@ and open the template in the editor.
     <body>
 <div class="bgimg w3-display-container w3-text-red">
   <div class="w3-display-middle w3-jumbo">
-      <img src="/PROJETO_LES/res/img/logo.jpg" alt="logo">
+      <img src="/PROJETO_LES/res/img/logo.jpg" style="width:100%" alt="logo">
   </div>
   <div class="w3-display-topleft w3-container w3-xlarge">
     <p><button onclick="document.getElementById('menu').style.display='block'" class="w3-button w3-red">login</button></p>
@@ -44,8 +45,25 @@ and open the template in the editor.
       <span onclick="document.getElementById('menu').style.display='none'" class="w3-button w3-display-topright w3-large">x</span>
       <h1>Login </h1>
     </div>
-    <div class="w3-container">
-      <h5>Form de login aqui</h5>
+    <div class="w3-center w3-card-4">
+      <p>Acesse sua conta Gameet!<p>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+          <h4 class="w3-text-red"><b>Usuário</b></h4>
+            <div class="w3-row w3-section w3-text-red">
+                <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
+                <div class="w3-rest">
+                    <input class="w3-input w3-padding-16 w3-border w3-border-red w3-round-xxlarge" name="username" type="text" placeholder="Usuário">
+                </div>
+            </div>
+          <h4 class="w3-text-red"><b>Senha</b></h4>
+          <div class="w3-row w3-section w3-text-red">
+                <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-key"></i></div>
+                <div class="w3-rest">
+          <input class="w3-input w3-padding-16 w3-border w3-border-red w3-round-xxlarge" type="text" placeholder="Senha" required name="password">
+                          </div>
+            </div>
+          <p><button class="w3-button w3-red w3-round-xxlarge" style="width:50%" type="submit">LOGAR</button></p><br>
+      </form>
     </div>
   </div>
 </div>
