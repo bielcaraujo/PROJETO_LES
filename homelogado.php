cf395effb7a1bf16eb39f6cdbf8a01c841b2a66c
Danilo <Danilo@192.168.12.92>
2018-04-18 16:32:30 -0300

    front end do home logado

---
+++ homelogado.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+<head>  
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title></title>  
+</head>
+<body>
+  <div class="w3-bar w3-purple w3-border w3-padding">
+    <input type="text" class="w3-bar-item w3-input w3-white w3-mobile" style="width: 300px;" placeholder="Pesquisa pessoas, jogos, etc...">
+    <a href="#" class="w3-bar-item w3-button" ><i class="fa fa-search"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-mobile w3-right">Sair</a>
+    <a href="#" class="w3-bar-item w3-button w3-mobile w3-right">Ranking</a>
+    </div>  
+<div class="w3-row">
+  <div class="w3-col s3 w3-center w3-purple"><img style="width: 40%;" src="nah.png">
+      </br>
+    <div class="w3-panel w3-white">
+  <h3 class="w3-opacity">
+  <b>Nome: Nah</b></h3>
+  <h4 class="w3-opacity"><b>3000 anos</b></h4></b>
+  <h5>Jogos:</h5>
+  <h6>league of legends</h6>
+  <h6>overwatch</h6>
+  <h6>tristeza online</h6>
+  </div>
+  </div>
+      
+  <div class="w3-col s9 w3-center">
+   <div class="w3-card-4 w3-white" style="width:30%" style="padding:10px>
+
+    <div class="w3-container w3-center">
+      <h3>Friend Request</h3>
+      <img src="nah2.png" alt="Avatar" style="width:50%">
+      <h5>Robin</h5>
+
+      <div class="w3-section">
+        <button class="w3-button w3-green">Accept</button>
+        <button class="w3-button w3-red">Decline</button>
+      </div>
+    </div>
+
+  </div>
+    
+  </div>
+</div>
+</body>
+</html>
